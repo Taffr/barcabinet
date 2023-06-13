4320bd7bab871fd1810112f3866787c0cc50806e
Simon Tenggren <simon.tenggren@gmail.com>
2023-06-13 10:40:47 +0200

    chore(db): add docker file and init script

---
+++ db/init.js
@@ -0,0 +1,11 @@
+const ingredientsIndex = {
+  'name': 1,
+  'ingredients.name': 1,
+}
+
+db = db.getSiblingDB('barcabinet')
+db.createCollection('recipes')
+db.recipes.createIndex(ingredientsIndex)
+
+load('/docker-entrypoint-initdb.d/cocktails.json')
+
