a77f10f5411e816bfff24dcf25cf215c43085ac2
Simon Tenggren <simon.tenggren@gmail.com>
2023-06-13 10:58:23 +0200

    fix(db): add id to ingredients

--- db/convert_old_format.js
+++ db/convert_old_format.js
@@ -2,10 +2,18 @@ import { readFileSync, writeFileSync } from 'fs'
 
 function main() {
   const oldRecipes = JSON.parse(readFileSync('./cocktails_2.0.json', 'utf-8'))
+  const allIngredients = [ ... new Set(oldRecipes.flatMap((r) => r.Ingredients).map((i) => i.Name)) ]
+  let counter = 0
+  const ingredientNameIdMap = allIngredients.reduce((acc, iName) => {
+      acc[iName] = counter++
+      return acc
+  }, {})
+
   const newRecipes = oldRecipes.map((r) => {
     const { Name, Preparation, Garnish, Ingredients } = r 
 
     const convertedIngredients = Ingredients.map((i) => ({
+      id: ingredientNameIdMap[i.Name],
       name: i.Name,
       dosage: i.Dosage,
       unit: i.Unit
