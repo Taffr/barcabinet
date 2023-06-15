d669ae9aefea253d849100a938251619d758e67f
Simon Tenggren <simon.tenggren@gmail.com>
2023-06-15 14:16:22 +0200

    feat(api): add cors

--- api/server.ts
+++ api/server.ts
@@ -1,12 +1,13 @@
 import express, { Express } from 'express'
 import bodyParser from 'body-parser'
+import cors from 'cors'
 import { registerAllQueries } from './src/rest/register-all-queries.js'
 import { registerAllCommands } from './src/rest/register-all-commands.js'
 import { compose } from 'ramda'
-
 const app: Express = express()
 const port = process.env.port || 3000
 
+app.use(cors())
 app.use(bodyParser.json())
 
 const registeredApp = compose(registerAllQueries, registerAllCommands)(app)
