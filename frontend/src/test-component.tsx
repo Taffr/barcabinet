92edac45291cb34cff872b08cae23529729ede7f
Simon Tenggren <simon.tenggren@gmail.com>
2023-06-15 09:56:22 +0200

    feat(fe): add mui component lib

--- frontend/src/test-component.tsx
+++ frontend/src/test-component.tsx
@@ -1,8 +1,19 @@
+import { useState } from 'react'
+import Button from '@mui/material/Button'
+import { inc } from 'ramda'
 
-export function TestComponent (props: { message: string }) {
-    return (
-        <h1>
-            { props.message }
-        </h1>
-    )
+export function TestComponent () {
+  const [ count, setCount ] = useState(0)
+
+  return (
+    <div>
+      <Button
+        variant='contained'
+        onClick={ () => setCount(inc)}
+      >
+      Count: { count } 
+      </Button>
+      { count === 69 && <h1> Nice </h1> }
+    </div>
+  )
 } 
