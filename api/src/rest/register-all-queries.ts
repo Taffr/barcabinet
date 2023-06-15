846de300ef96e604bd12aee9987dd9d2da3db804
Simon Tenggren <simon.tenggren@gmail.com>
2023-06-15 09:18:32 +0200

    fix(api): remove glob dependency

--- api/src/rest/register-all-queries.ts
+++ api/src/rest/register-all-queries.ts
@@ -2,13 +2,17 @@ import { registerQuery } from './register-query.js'
 import type { Express } from 'express'
 import { compose, map, reduce, replace } from 'ramda'
 import bluebird from 'bluebird'
-import { glob } from 'glob'
+
+const allFilePaths = [
+  '../queries/recipes/query-get-all-recipes.js',
+  '../queries/ingredients/query-get-all-ingredients.js',
+] as const
 
 const formatToRelativeJsImport =
   compose(replace('.ts', '.js'), replace('src/', '../'))
 
-const queryFiles = await glob('./src/**/query-*.ts')
-const formatted = map(formatToRelativeJsImport, queryFiles)
+
+const formatted = map(formatToRelativeJsImport, allFilePaths)
 const allQueries = await bluebird.map(formatted, async (path) => {
   return (await import(path)).default
 })
