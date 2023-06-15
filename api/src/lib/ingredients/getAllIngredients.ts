8190ec0063b811144ca6cd683b48cbcc0ec9e499
Simon Tenggren <simon.tenggren@gmail.com>
2023-06-15 14:46:47 +0200

    fix(api): get only unique ingredients

--- api/src/lib/ingredients/getAllIngredients.ts
+++ api/src/lib/ingredients/getAllIngredients.ts
@@ -1,12 +1,12 @@
 import { recipeCollection, RecipeCollection } from '../../db/collections/recipe-collection.js'
 import { Ingredient } from '../../types/ingredient.js'
-import { chain, prop, uniq } from 'ramda'
+import { chain, prop, uniqBy } from 'ramda'
 
 export function factory (recipeCollection: RecipeCollection) {
   return function getAllIngredients (): Promise<Ingredient[]> {
     return recipeCollection.find()
       .then(chain(prop('ingredients')))
-      .then(uniq)
+      .then(uniqBy(prop('id')))
   }
 }
 export const getAllIngredients = factory(recipeCollection)
