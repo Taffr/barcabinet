901fea291b948755f5ec615a2379d34e0c077091
Simon Tenggren <simon.tenggren@gmail.com>
2023-06-15 09:56:22 +0200

    feat(fe): add mui component lib

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,14 @@
 import './App.css'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 import { TestComponent } from './test-component.tsx'
 
 function App() {
     return (
-        <TestComponent
-            message="Hello there!"
-        />
+        <TestComponent/>
   )
 }
 
