7beef7678d85dba81483b528f439094f712b002c
Simon Tenggren <simon.tenggren@gmail.com>
2023-07-10 10:53:49 +0200

    chore(api): migrate to nest.js
    Removes all files and uses basic nest.js structure after creating files

--- api/acceptance/support/hooks.ts
+++
@@ -1,6 +0,0 @@
-import { AfterAll, BeforeAll, setWorldConstructor } from '@cucumber/cucumber'
-import { TestWorld } from './test-world.js'
-import { MongoConnection } from '../../src/db/mongo-connection.js'
-
-BeforeAll(() => setWorldConstructor(TestWorld))
-AfterAll(async () => await MongoConnection.getInstance().disconnect())
