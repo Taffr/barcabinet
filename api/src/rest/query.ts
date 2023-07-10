00abf08749541542cfdfa12219681e6474f7bdf9
Simon Tenggren <simon.tenggren@gmail.com>
2023-07-10 10:53:49 +0200

    chore(api): migrate to nest.js
    Removes all files and uses basic nest.js structure after creating files

--- api/src/rest/query.ts
+++
@@ -1,8 +0,0 @@
-import { Request } from 'express'
-
-export interface Query<ProcessedParams, QueryResult> {
-  statusCode: number
-  route: string
-  processParams?: (req: Request) => Promise<ProcessedParams>
-  runQuery: (params?: ProcessedParams) => Promise<QueryResult>
-}
