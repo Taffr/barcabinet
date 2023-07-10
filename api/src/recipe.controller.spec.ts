45d3523f5f236b9c95c911a8ec44e2ddce02fece
Simon Tenggren <simon.tenggren@gmail.com>
2023-07-10 14:35:31 +0200

    chore(api): use mock in test

--- api/src/recipe.controller.spec.ts
+++ api/src/recipe.controller.spec.ts
@@ -1,15 +1,37 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { RecipeController } from './recipe.controller';
-import { AppService } from './app.service';
 import { RecipeStore } from './recipestore.service';
 
 describe('RecipeController', () => {
   let recipeController: RecipeController;
 
+  const mockRecipeStore = {
+    getAll() {
+      return [
+        {
+          name: 'A Cocktail',
+        },
+      ];
+    },
+    containingIngredient(ingredientId: number) {
+      return [
+        {
+          name: 'Another Cocktail',
+          ingredients: [{ id: ingredientId }],
+        },
+      ];
+    },
+  };
+
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [RecipeController],
-      providers: [AppService, RecipeStore],
+      providers: [
+        {
+          provide: RecipeStore,
+          useValue: mockRecipeStore,
+        },
+      ],
     }).compile();
 
     recipeController = app.get<RecipeController>(RecipeController);
@@ -17,19 +39,17 @@ describe('RecipeController', () => {
 
   describe('getAll()', () => {
     it('should return all recipes', () => {
-      expect(recipeController.getAll().length).toBeGreaterThan(1);
+      expect(recipeController.getAll().length).toBeGreaterThan(0);
     });
   });
 
   describe('containingIngredient()', () => {
-    it('should return nothing for non exisiting ingredient id', () => {
-      expect(recipeController.containingIngredient('-1')).toStrictEqual([]);
-    });
-
     it('should return a recipe when asking for an ingredient', () => {
       expect(
-        recipeController.containingIngredient('0').length,
-      ).toBeGreaterThanOrEqual(1);
+        recipeController
+          .containingIngredient('0')[0]
+          .ingredients.some(({ id }) => id === 0),
+      ).toBe(true);
     });
   });
 });
