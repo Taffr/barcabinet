c787f04c8a054053f3b3267bef50175032126ce1
Simon Tenggren <simon.tenggren@gmail.com>
2023-07-12 13:59:18 +0200

    chore(api): dockerize and deply api

--- api/src/main.ts
+++ api/src/main.ts
@@ -4,6 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app: NestExpressApplication =
     await NestFactory.create<NestExpressApplication>(AppModule, { cors: true });
-  await app.listen(3000);
+  const port = process.env.PORT || 8080;
+  console.log(`Starting API on ${port}`);
+  await app.listen(port);
 }
 bootstrap();
