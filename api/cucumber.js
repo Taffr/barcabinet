1f637731843beb19d067dd6d6328402165071dff
Simon Tenggren <simon.tenggren@gmail.com>
2023-07-14 14:38:21 +0200

    feat(ci): add acceptance tests to CI

--- api/cucumber.js
+++ api/cucumber.js
@@ -4,7 +4,6 @@ const config = [
   '--require test/acceptance/step-definitions/**/*.ts', // Load TypeScript module
   '--require test/acceptance/support/**/*.ts', // Load TypeScript module
   '--require test/acceptance/hooks/**/*.ts', // Load TypeScript module
-  '--format progress-bar', // Progress bar
   '--publish-quiet', // Hush
 ].join(' ');
 
