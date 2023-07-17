3040f3300410276cd54101b825c7d0f92e0e7b40
Simon Tenggren <simon.tenggren@gmail.com>
2023-07-17 13:00:16 +0200

    feat(api): added Maybe type

---
+++ api/src/util/Maybe.ts
@@ -0,0 +1,52 @@
+import { curry, curryN } from 'ramda';
+
+export enum MaybeType {
+  Just = 'maybe-type_just',
+  Nothing = 'maybe-type_nothing',
+}
+
+interface Just<T> {
+  type: typeof MaybeType.Just;
+  value: T;
+}
+
+interface Nothing {
+  type: typeof MaybeType.Nothing;
+}
+
+type Maybe<T> = Just<T> | Nothing;
+
+const Nothing = (): Nothing => ({ type: MaybeType.Nothing });
+
+const Just = <T>(v: T): Just<T> => ({ type: MaybeType.Just, value: v });
+
+function maybeOf<T>(value: T): Maybe<T> {
+  if (value === undefined || value === null) {
+    return Nothing();
+  }
+  return Just(value);
+}
+
+function maybeMap<A, B>(f: (val: A) => B, m: Maybe<A>): Maybe<B> {
+  switch (m.type) {
+    case MaybeType.Nothing:
+      return Nothing();
+    default:
+      return Just(f(m.value));
+  }
+}
+
+const maybeChain = <A, B>(f: (val: A) => Maybe<B>, m: Maybe<A>): Maybe<B> => {
+  switch (m.type) {
+    case MaybeType.Nothing:
+      return Nothing();
+    default:
+      return f(m.value);
+  }
+};
+
+export const Maybe = {
+  of: maybeOf,
+  map: curry(maybeMap),
+  chain: curry(maybeChain),
+};
