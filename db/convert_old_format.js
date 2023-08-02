2f054fb8df99d383e1251475c2ebad5c638b1d15
Simon Tenggren <simon.tenggren@gmail.com>
2023-08-02 12:22:27 +0200

    chore(db): clean up recipe data

--- db/convert_old_format.js
+++ db/convert_old_format.js
@@ -1,12 +1,26 @@
 import { readFileSync, writeFileSync } from 'fs'
+import { v4 as uuidv4 } from 'uuid';
 
 function main() {
   const oldRecipes = JSON.parse(readFileSync('./cocktails_2.0.json', 'utf-8'))
+
   const allIngredients = [ ... new Set(oldRecipes.flatMap((r) => r.Ingredients).map((i) => i.Name)) ]
+  const ingredientFrequency = {}
+  
+  // oldRecipes
+  //   .flatMap((r) => r.Ingredients)
+  //   .forEach((i) => {
+  //     const curr = ingredientFrequency[i.Name]
+  //     ingredientFrequency[i.Name] = curr ? curr + 1 : 1
+  //   })
+
+  // const lowFrequency = Object.entries(ingredientFrequency).filter(([ key, value ]) => value < 3)
+  // console.dir(lowFrequency, { depth: null })
+  
   let counter = 0
   const ingredientNameIdMap = allIngredients.reduce((acc, iName) => {
-      acc[iName] = counter++
-      return acc
+    acc[iName] = counter++
+    return acc
   }, {})
 
   const newRecipes = oldRecipes.map((r) => {
@@ -15,13 +29,14 @@ function main() {
     const convertedIngredients = Ingredients.map((i) => ({
       id: ingredientNameIdMap[i.Name],
       name: i.Name,
-      dosage: i.Dosage,
-      unit: i.Unit
+      ...(i.Dosage && { dosage: i.Dosage }),
+      ...(i.Unit && { unit: i.Unit }),
     }))
 
     return {
+      id: uuidv4(),
       name: Name,
-      preparation: Preparation,
+      ...(Preparation.length !== 0 && { preparation: Preparation }),
       ...(Garnish.length !== 0 && { garnish: Garnish }),
       ingredients: convertedIngredients,
     }
