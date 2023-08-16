f0828a5630a41086286139a36c84462b1e540e0b
Simon Tenggren <simon.tenggren@gmail.com>
2023-08-16 15:00:02 +0200

    feat(frontend): render only some recipes, load more on scroll
    Only displays a default of 20 recipes in the recipe view, but adds 5
    more each time you scroll to the bottom.

--- frontend/src/components/recipe-filter-controls.tsx
+++ frontend/src/components/recipe-filter-controls.tsx
@@ -1,18 +1,29 @@
 import {
-  Box,
+  FormControl,
   FormControlLabel,
+  InputLabel,
   MenuItem,
   Select,
   Stack,
   Switch,
 } from '@mui/material';
-import { BaseSyntheticEvent } from 'react';
+import { BaseSyntheticEvent, useState } from 'react';
+
+const SORTING_VALUES = {
+  NO_SORT: '',
+  NUM_ING_OWNED: 0,
+  NUM_ING_ASC: 1,
+  NUM_ING_DESC: 2,
+} as const;
 
 export const RecipeFilterControls = (props: {
   onFavouriteFilterToggled: (isSwitched: boolean) => void;
   onIngredientFilterToggled: (isSwitched: boolean) => void;
 }) => {
   const { onFavouriteFilterToggled, onIngredientFilterToggled } = props;
+  const [sortingValue, setSortingValue] = useState<number | string>(
+    SORTING_VALUES.NO_SORT,
+  );
 
   const handleOnlyFavourites = (e: BaseSyntheticEvent) =>
     onFavouriteFilterToggled(e.target.checked);
@@ -21,22 +32,43 @@ export const RecipeFilterControls = (props: {
     onIngredientFilterToggled(e.target.checked);
 
   return (
-    <Box display="flex" justifyContent="right" justifyItems="center">
-      <Stack direction="row" alignItems="center" spacing={10}>
-        <Select value={10} label="Sort By">
-          <MenuItem> # Ingredients owned </MenuItem>
-          <MenuItem> # Ingredients (ascending) </MenuItem>
-          <MenuItem> # Ingredients (descending) </MenuItem>
+    <Stack direction="row" spacing={12} justifyContent="center">
+      <FormControl>
+        <InputLabel id="sorting-select-label-id">Sort By</InputLabel>
+        <Select
+          id="sorting-select"
+          sx={{ width: '14vw' }}
+          labelId="sorting-select-label-id"
+          label="Sort By"
+          value={sortingValue}
+          onChange={(e) => setSortingValue(e.target.value)}
+        >
+          <MenuItem value={SORTING_VALUES.NO_SORT}>
+            {' '}
+            <em> None </em>{' '}
+          </MenuItem>
+          <MenuItem value={SORTING_VALUES.NUM_ING_OWNED}>
+            {' '}
+            # Ingredients owned{' '}
+          </MenuItem>
+          <MenuItem value={SORTING_VALUES.NUM_ING_ASC}>
+            {' '}
+            # Ingredients (ascending){' '}
+          </MenuItem>
+          <MenuItem value={SORTING_VALUES.NUM_ING_DESC}>
+            {' '}
+            # Ingredients (descending){' '}
+          </MenuItem>
         </Select>
-        <FormControlLabel
-          control={<Switch onChange={handleOnlyFavourites} />}
-          label="Include only favourites"
-        />
-        <FormControlLabel
-          control={<Switch onChange={handleToggleExcludeIngredients} />}
-          label="Include only with selected ingredients"
-        />
-      </Stack>
-    </Box>
+      </FormControl>
+      <FormControlLabel
+        control={<Switch onChange={handleOnlyFavourites} />}
+        label="Include only favourites"
+      />
+      <FormControlLabel
+        control={<Switch onChange={handleToggleExcludeIngredients} />}
+        label="Include only with selected ingredients"
+      />
+    </Stack>
   );
 };
